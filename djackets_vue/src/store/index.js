400ef0947a0896a5dda76467a3a869b591ca5760
Sirius-Star42 <sirius.star4242@gmail.com>
2021-11-27 14:26:52 +0300

    added cart

--- djackets_vue/src/store/index.js
+++ djackets_vue/src/store/index.js
@@ -2,8 +2,32 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    cart: {
+      items: []
+    },
+    isAuthanticate: false,
+    token: '',
+    isLoading: false,
+
   },
   mutations: {
+    initializeStore(state) {
+      if(localStorage.getItem('cart')) {
+        state.cart = JSON.parse(localStorage.getItem('cart'))
+      } else {
+        localStorage.setItem('cart', JSON.stringify(state.cart))
+      }
+    },
+    addToCart(state, item) {
+      const exits = state.cart.items.filter(i => i.product.id === item.product.id)
+
+      if (exits.length) {
+        exits[0].quantity = parseInt(exits[0].quantity) + parseInt(item.quantity)
+      } else {
+        state.cart.items.push(item)
+      }
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    }
   },
   actions: {
   },
--- djackets_vue/src/views/Product.vue
+++ djackets_vue/src/views/Product.vue
@@ -17,7 +17,7 @@
                         <input type="number" class="input" min="1" v-model="quantity">
                     </div>
                     <div class="control">
-                        <a class="button is-dark">Add to cart</a>
+                        <a class="button is-dark" @click="addToCart">Add to cart</a>
                     </div>
                 </div>
             </div>
@@ -53,14 +53,22 @@ import axios from 'axios';
                     .catch(error => {
                         console.log(error.message)
                     })
+            },
+            addToCart() {
+                if (isNaN(this.quantity) || this.quantity < 1) {
+                    this.quantity = 1
+                }
+
+                const item = {
+                    product: this.product,
+                    quantity: this.quantity
+                }
+
+                this.$store.commit('addToCart', item)
             }
         },
     }
 </script>
 <style scoped>
-/* img {
-    height: 20vh;
-    width: auto;
-} */
 
 </style>
